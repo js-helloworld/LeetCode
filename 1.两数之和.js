ab764cad4fccf887c8380fa9550d65b4b3f1366f
zongxuan <js_helloworld@163.com>
2024-11-01 13:42:54 +0800

    fix: 添加描述文件

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -2,6 +2,10 @@
  * @lc app=leetcode.cn id=1 lang=javascript
  *
  * [1] 两数之和
+ *
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+ * 你可以按任意顺序返回答案。
  */
 
 // @lc code=start
@@ -19,5 +23,4 @@ var twoSum = function(nums, target) {
     }
     numsMap.set(nums[i],i)
   }
-
 };
